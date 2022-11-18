18b38015a0eb1420db2e4879164d80d3a04c62aa
ZikaZaki <zika.github@gmail.com>
2022-11-18 23:35:09 +0300

    Fix linter errors

--- nameable.rb
+++ nameable.rb
@@ -1,5 +1,5 @@
 class Nameable
-    def correct_name
-        raise NotImplementedError, "Please implement the #{self.class}##{__method__} method"
-    end
+  def correct_name
+    raise NotImplementedError, "Please implement the #{self.class}##{__method__} method"
+  end
 end
