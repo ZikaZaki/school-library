541c1c8c037f6b55602e8cd29a9b8a9b6e9b2283
Elfin Git <musongoletracy@gmail.com>
2022-11-22 19:57:03 +0200

    refactor menu options to main.rb file

--- app.rb
+++ app.rb
@@ -90,17 +90,6 @@ class App
     end
   end
 
-  def display_menu
-    puts 'Please choose an option by entering a number:'
-    puts '1 - List all books'
-    puts '2 - List all people'
-    puts '3 - Create a person'
-    puts '4 - Create a book'
-    puts '5 - Create a rental'
-    puts '6 - List all rentals for a given person id'
-    puts '7 - Exit'
-  end
-
   def process_option(option)
     case option
     when '1'
--- main.rb
+++ main.rb
@@ -6,6 +6,17 @@ def main
   puts "|\tWelcome to School Library App!\t\t |"
   puts '-' * 50
   app.run
+  
+  def display_menu
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person id'
+    puts '7 - Exit'
+  end
 end
 
 main
