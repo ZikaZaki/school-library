7f8f3f4b980393c74eab1c4ed013627652b8fb64
ZikaZaki <zika.github@gmail.com>
2022-11-25 01:11:02 +0300

    Add load_data & save_data methods

--- app.rb
+++ app.rb
@@ -5,11 +5,32 @@ require_relative 'teacher'
 require_relative 'book'
 require_relative 'rental'
 
-class App
+class App 
   def initialize
     @people = []
     @books = []
     @rentals = []
+    load_data
+  end
+
+  def load_data
+    File.exist?('./data/books.json') ? 
+      @books = JSON.parse(File.read('./data/books.json')).map { |book| Book.new(book['title'], book['author'], id: book['id']) }
+      : File.open('./data/books.json', 'w') { |file| file.write(JSON.dump([])) }
+
+    File.exist?('./data/people.json') ?
+      @people = JSON.parse(File.read('./data/people.json')).map do |person|
+        person['type'] == 'Student' ?
+        Student.new(person['age'], id: person['id'], name: person['name'], parent_permission: person['parent_permission'], classroom: person['classroom'])
+        : Teacher.new(person['age'], person['specialization'], id: person['id'], name: person['name'])
+      end
+      : File.open('./data/people.json', 'w') { |file| file.write(JSON.dump([])) }
+
+    File.exist?('./data/rentals.json') ?
+      @rentals = JSON.parse(File.read('./data/rentals.json')).map do |rental|
+        Rental.new(rental['date'], @books.find { |book| book.id == rental['book']['id'] }, @people.find { |person| person.id == rental['person']['id'] })
+      end
+      : File.open('./data/rentals.json', 'w') { |file| file.write(JSON.dump([])) }
   end
 
   def create_book
@@ -60,12 +81,12 @@ class App
   def create_rental
     puts 'Select a book from the following list by number'
     @books.each_with_index do |book, index|
-      puts "#{index}) Title: '#{book.title}', Author: #{book.author}"
+      puts "#{index}) [#{book.class}] ID: #{book.id}, Title: '#{book.title}', Author: #{book.author}"
     end
     book_index = gets.chomp.to_i
     puts 'Select a person from the following list by number (not id)'
     @people.each_with_index do |person, index|
-      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
+      puts "#{index}) [#{person.class}] ID: #{person.id}, Name: #{person.name}, Age: #{person.age}"
     end
     person_index = gets.chomp.to_i
     puts 'Date:'
@@ -79,7 +100,7 @@ class App
   end
 
   def list_books
-    @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
+    @books.each { |book| puts "ID: #{book.id}, Title: #{book.title}, Author: #{book.author}" }
   end
 
   def list_rentals_for_person_id
@@ -87,8 +108,22 @@ class App
     id = gets.chomp.to_i
     puts 'Rentals:'
     @rentals.each do |rental|
-      puts "Date: #{rental.date}, Book '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
+      puts "Date: #{rental.date}, [##{rental.book.id}]-Book '#{rental.book.title}' by #{rental.book.author}" if rental.person.id == id
     end
+  end
+
+  def save_data
+    @books.size > 0 ?
+      File.open('./data/books.json', 'w') { |file| file.write(JSON.dump(@books)) }
+      : File.open('./data/books.json', 'w') { |file| file.write(JSON.dump([])) }
+
+    @people.size > 0 ?
+      File.open('./data/people.json', 'w') { |file| file.write(JSON.dump(@people)) }
+      : File.open('./data/people.json', 'w') { |file| file.write(JSON.dump([])) }
+
+    @rentals.size > 0 ?
+      File.open('./data/rentals.json', 'w') { |file| file.write(JSON.dump(@rentals)) }
+      : File.open('./data/rentals.json', 'w') { |file| file.write(JSON.dump([])) }
   end
 
   def process_option(option)
